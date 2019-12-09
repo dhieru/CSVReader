ea004efa40506fd014ee651d4317635a65cff5b8
dhieru <dhiru.kumar.singh@gmail.com>
2019-12-08 18:47:41 -0700

    Added the main function

--- src/com/dhiru/converter/JavaToCSV.java
+++ src/com/dhiru/converter/JavaToCSV.java
@@ -1,6 +1,9 @@
 package com.dhiru.converter;
 
 public class JavaToCSV {
-	
+	public static void main (String [] args){
+        String csvFilePath = "";
+        BufferedReader br = null;
+    }
 
 }
