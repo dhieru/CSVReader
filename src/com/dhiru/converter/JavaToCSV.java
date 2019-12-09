b2265dd367c0a6d1564f086efe61790f0fd38ec7
dhieru <dhiru.kumar.singh@gmail.com>
2019-12-08 18:52:12 -0700

    Added the try catch block for error handling

--- src/com/dhiru/converter/JavaToCSV.java
+++ src/com/dhiru/converter/JavaToCSV.java
@@ -7,13 +7,28 @@ public class JavaToCSV {
         String line = "";
         String cvsSplitBy = ",";
 
-        br = new BufferedReader(new FileReader(csvFile));
+        
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
             while ((line = br.readLine()) != null) {
 
                 // use comma as separator
                 String[] country = line.split(cvsSplitBy);
 
                 System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+        }
+          }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
     }
-
+}
 }
