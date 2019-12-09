1e14d204f8c8fe1b4349e86c65470b9221e0f943
dhieru <dhiru.kumar.singh@gmail.com>
2019-12-08 18:49:55 -0700

    Add the buisness logic to read the csv file

--- src/com/dhiru/converter/JavaToCSV.java
+++ src/com/dhiru/converter/JavaToCSV.java
@@ -4,6 +4,16 @@ public class JavaToCSV {
 	public static void main (String [] args){
         String csvFilePath = "";
         BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+
+        br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] country = line.split(cvsSplitBy);
+
+                System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
     }
 
 }
